93d5dd94f70e249780edcc245527744f29787205
amrbakoubann <abako054@uottawa.ca>
2023-11-13 10:31:55 -0500

    Created lol_playtime_checker.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import requests
+
+# Set your Riot Games API key here
+API_KEY = 'YOUR_API_KEY'
+RIOT_API_BASE_URL = 'https://<region>.api.riotgames.com/lol/'  # Replace <region> with the appropriate region code
---
+++ riot_api.py
@@ -0,0 +1,30 @@
+import requests
+from config import API_KEY, RIOT_API_BASE_URL
+
+def get_summoner_id(summoner_name): #function to obtain summoner id 
+    url = f"{RIOT_API_BASE_URL}summoner/v4/summoners/by-name/{summoner_name}" #riot API URL
+    response = requests.get(url, headers={"X-Riot-Token": API_KEY})
+    
+    if response.status_code == 200:
+        return response.json()['id']
+    else:
+        print(f"Error fetching summoner ID: {response.status_code}")
+        return None
+
+def calculate_total_playtime(summoner_id): #function to calculate summoner playtime
+    url = f"{RIOT_API_BASE_URL}match/v4/matchlists/by-account/{summoner_id}"
+    response = requests.get(url, headers={"X-Riot-Token": API_KEY})
+
+    if response.status_code == 200:
+        matches = response.json()['matches']
+        total_playtime = sum([get_match_duration(match['gameId']) for match in matches])
+        return total_playtime
+    else:
+        print(f"Error fetching matchlist: {response.status_code}")
+        return 0
+
+# other Riot API related functions
+def get_match_duration(match_id):
+    # You'll need to implement this function to fetch match details
+    # and return the match duration
+    pass
