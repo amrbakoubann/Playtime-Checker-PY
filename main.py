cd09ea179241d059f23dc47edf25829215adf184
amrbakoubann <abako054@uottawa.ca>
2024-01-15 11:51:12 -0500

    fixed indentation

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ def main():
     if summoner_id:
         total_playtime = calculate_total_playtime(summoner_id)
         print(f" Your total playtime for {summoner_name}: {total_playtime} minutes")
-            if (total_playtime >= 50000)
-                print (f" Did you really waste over a month playing this trash? Get a life lol" )
-    else:
-        print("Error! Summoner not found.")
+        if (total_playtime >= 50000):
+            print (f" Did you really waste over a month playing this trash? Get a life lol" )
+        else:
+            print("Error! Summoner not found.")
 
 if __name__ == "__main__":
     main()
